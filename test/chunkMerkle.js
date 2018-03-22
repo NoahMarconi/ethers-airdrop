8a607927a12199f8735fd77777af247454a99301
Noah Marconi <noah.marconi@gmail.com>
2018-03-22 11:25:15 -0400

    update tests to use balances k/v format

--- test/chunkMerkle.js
+++ test/chunkMerkle.js
@@ -5,47 +5,101 @@ var AirDrop = require('..');
 
 describe('Chunking Merkle', function() {
     var exArray;
+    var exBalances;
+    var exLeaves;
+    var hashedLeaves;
+    var merkleRoot = '';
+    var mNodes = [];
+    var nNodes = [];
+    var oNodes = [];
+
 
     before(function() {
-        exArray = [1, 2, 3, 4, 5, 6].map(function(x) { return ethers.utils.keccak256('0x' + x); });
+        exArray = Array.apply(null, { length: 16 })
+            .map(Number.call, Number);
+
+        exBalances = exArray.reduce(function(acc, el) {
+            // Match airdrop-balances.json format with a random address for key and a hex string for value.
+            acc[ethers.Wallet.createRandom().address] = ethers.utils.bigNumberify(el + 1).toHexString();
+            return acc;
+        }, {});
+
+        exLeaves = AirDrop.merkleTools.expandLeaves(exBalances);
+        hashedLeaves = AirDrop.merkleTools.getLeaves(exBalances);
+
+
+        mNodes = [
+            ethers.utils.keccak256(hashedLeaves[0] + hashedLeaves[1].substring(2)),
+            ethers.utils.keccak256(hashedLeaves[2] + hashedLeaves[3].substring(2)),
+            ethers.utils.keccak256(hashedLeaves[4] + hashedLeaves[5].substring(2)),
+            ethers.utils.keccak256(hashedLeaves[6] + hashedLeaves[7].substring(2)),
+            ethers.utils.keccak256(hashedLeaves[8] + hashedLeaves[9].substring(2)),
+            ethers.utils.keccak256(hashedLeaves[10] + hashedLeaves[11].substring(2)),
+            ethers.utils.keccak256(hashedLeaves[12] + hashedLeaves[13].substring(2)),
+            ethers.utils.keccak256(hashedLeaves[14] + hashedLeaves[15].substring(2))
+        ];
+
+        nNodes = [
+            ethers.utils.keccak256(mNodes[0] + mNodes[1].substring(2)),
+            ethers.utils.keccak256(mNodes[2] + mNodes[3].substring(2)),
+            ethers.utils.keccak256(mNodes[4] + mNodes[5].substring(2)),
+            ethers.utils.keccak256(mNodes[6] + mNodes[7].substring(2)),
+        ];
+
+        oNodes = [
+            ethers.utils.keccak256(nNodes[0] + nNodes[1].substring(2)),
+            ethers.utils.keccak256(nNodes[2] + nNodes[3].substring(2))
+        ]
+
+        merkleRoot = ethers.utils.keccak256(oNodes[0] + oNodes[1].substring(2));
     });
 
     describe('reduceMerkleParents()', function() {
+        var smallArray = [];
+
+        before(function() {
+           smallArray = [1, 2, 3, 4, 5, 6].map(function(x) { return ethers.utils.keccak256('0x' + x); });
+        });
 
         it('should reduce array of leaf nodes to thier parent', function() {
             var parentHashes = [
-                ethers.utils.keccak256(exArray[0] + exArray[1].substring(2)),
-                ethers.utils.keccak256(exArray[2] + exArray[3].substring(2)),
-                ethers.utils.keccak256(exArray[4] + exArray[5].substring(2))
+                ethers.utils.keccak256(smallArray[0] + smallArray[1].substring(2)),
+                ethers.utils.keccak256(smallArray[2] + smallArray[3].substring(2)),
+                ethers.utils.keccak256(smallArray[4] + smallArray[5].substring(2))
             ];
 
-            assert.deepEqual(parentHashes, AirDrop.merkleTools.reduceMerkleParents(exArray));
+            assert.deepEqual(parentHashes, AirDrop.merkleTools.reduceMerkleParents(smallArray));
         });
 
         it('should hash single leaf nodes with self', function() {
             var parentHashes = [
-                ethers.utils.keccak256(exArray[0] + exArray[1].substring(2)),
-                ethers.utils.keccak256(exArray[2] + exArray[3].substring(2)),
+                ethers.utils.keccak256(smallArray[0] + smallArray[1].substring(2)),
+                ethers.utils.keccak256(smallArray[2] + smallArray[3].substring(2)),
                 // Final element hashed with self.
-                ethers.utils.keccak256(exArray[4] + exArray[4].substring(2))
+                ethers.utils.keccak256(smallArray[4] + smallArray[4].substring(2))
             ];
 
             // Slice to produce odd length array.
-            assert.deepEqual(parentHashes, AirDrop.merkleTools.reduceMerkleParents(exArray.slice(0, 5)));
+            assert.deepEqual(parentHashes, AirDrop.merkleTools.reduceMerkleParents(smallArray.slice(0, 5)));
         });
 
         it('should has array of 2 into merkle root', function() {
-            assert.deepEqual([ethers.utils.keccak256(exArray[0] + exArray[1].substring(2))], AirDrop.merkleTools.reduceMerkleParents(exArray.slice(0, 2)));
+            assert.deepEqual([ethers.utils.keccak256(smallArray[0] + smallArray[1].substring(2))], AirDrop.merkleTools.reduceMerkleParents(smallArray.slice(0, 2)));
         });
     });
 
     describe('reduceMerkleRoot()', function() {
+        var smallArray = [];
+
+        before(function() {
+            smallArray = [1, 2, 3, 4, 5, 6].map(function(x) { return ethers.utils.keccak256('0x' + x); });
+         });
 
         it('should reduce array of leaf nodes to thier root hash', function() {
             var parentHashes = [
-                ethers.utils.keccak256(exArray[0] + exArray[1].substring(2)),
-                ethers.utils.keccak256(exArray[2] + exArray[3].substring(2)),
-                ethers.utils.keccak256(exArray[4] + exArray[5].substring(2))
+                ethers.utils.keccak256(smallArray[0] + smallArray[1].substring(2)),
+                ethers.utils.keccak256(smallArray[2] + smallArray[3].substring(2)),
+                ethers.utils.keccak256(smallArray[4] + smallArray[5].substring(2))
             ];
 
             var grandParentHashes = [
@@ -55,18 +109,13 @@ describe('Chunking Merkle', function() {
 
             var rootHash = ethers.utils.keccak256(grandParentHashes[0] + grandParentHashes[1].substring(2));
 
-            assert.deepEqual(rootHash, AirDrop.merkleTools.reduceMerkleRoot(exArray));
+            assert.deepEqual(rootHash, AirDrop.merkleTools.reduceMerkleRoot(smallArray));
         });
     });
 
     describe('chunkMerkleTree()', function() {
         var idx = 10;
-        var merkleRoot = '';
-        var exArray = [];
         var proof_0 = [];
-        var mNodes = [];
-        var nNodes = [];
-        var oNodes = [];
 
         var result_0 = {};
         var result_1 = {};
@@ -74,46 +123,17 @@ describe('Chunking Merkle', function() {
         var result_3 = {};
 
         before(function() {
-
-            exArray = Array.apply(null, { length: 16 })
-                .map(Number.call, Number)
-                .map(function(x) { return ethers.utils.keccak256('0x' + x); });
         
-            result_0 = AirDrop.merkleTools.chunkMerkleTree(exArray, 2);
-            result_1 = AirDrop.merkleTools.chunkMerkleTree(exArray, 4);
-            result_2 = AirDrop.merkleTools.chunkMerkleTree(exArray, 8);
-            result_3 = AirDrop.merkleTools.chunkMerkleTree(exArray, 16);
-
-            mNodes = [
-                ethers.utils.keccak256(exArray[0] + exArray[1].substring(2)),
-                ethers.utils.keccak256(exArray[2] + exArray[3].substring(2)),
-                ethers.utils.keccak256(exArray[4] + exArray[5].substring(2)),
-                ethers.utils.keccak256(exArray[6] + exArray[7].substring(2)),
-                ethers.utils.keccak256(exArray[8] + exArray[9].substring(2)),
-                ethers.utils.keccak256(exArray[10] + exArray[11].substring(2)),
-                ethers.utils.keccak256(exArray[12] + exArray[13].substring(2)),
-                ethers.utils.keccak256(exArray[14] + exArray[15].substring(2))
-            ];
-
-            nNodes = [
-                ethers.utils.keccak256(mNodes[0] + mNodes[1].substring(2)),
-                ethers.utils.keccak256(mNodes[2] + mNodes[3].substring(2)),
-                ethers.utils.keccak256(mNodes[4] + mNodes[5].substring(2)),
-                ethers.utils.keccak256(mNodes[6] + mNodes[7].substring(2)),
-            ];
-
-            oNodes = [
-                ethers.utils.keccak256(nNodes[0] + nNodes[1].substring(2)),
-                ethers.utils.keccak256(nNodes[2] + nNodes[3].substring(2))
-            ]
-
-            merkleRoot = ethers.utils.keccak256(oNodes[0] + oNodes[1].substring(2));
+            result_0 = AirDrop.merkleTools.chunkMerkleTree(hashedLeaves, 2);
+            result_1 = AirDrop.merkleTools.chunkMerkleTree(hashedLeaves, 4);
+            result_2 = AirDrop.merkleTools.chunkMerkleTree(hashedLeaves, 8);
+            result_3 = AirDrop.merkleTools.chunkMerkleTree(hashedLeaves, 16);
         
         });
 
         it('should throw if C not a power of 2', function() {
-            assert.throw(function() { AirDrop.merkleTools.chunckMerkleTree(exArray, 3) });
-            assert.doesNotThrow(function() { AirDrop.merkleTools.chunkMerkleTree(exArray, 8) });
+            assert.throw(function() { AirDrop.merkleTools.chunckMerkleTree(hashedLeaves, 3) });
+            assert.doesNotThrow(function() { AirDrop.merkleTools.chunkMerkleTree(hashedLeaves, 8) });
         });
 
         it('should chunk into expected number of leafs and roots', function() {
@@ -149,16 +169,11 @@ describe('Chunking Merkle', function() {
         var result_3 = {};
 
         before(function() {
-
-            exArray = Array.apply(null, { length: 16 })
-                .map(Number.call, Number)
-                .map(function(x) { return ethers.utils.keccak256('0x' + x); });
         
-            result_0 = AirDrop.merkleTools.chunkMerkleTree(exArray, 2);
-            result_1 = AirDrop.merkleTools.chunkMerkleTree(exArray, 4);
-            result_2 = AirDrop.merkleTools.chunkMerkleTree(exArray, 8);
-            result_3 = AirDrop.merkleTools.chunkMerkleTree(exArray, 16);
-        
+            result_0 = AirDrop.merkleTools.chunkMerkleTree(hashedLeaves, 2);
+            result_1 = AirDrop.merkleTools.chunkMerkleTree(hashedLeaves, 4);
+            result_2 = AirDrop.merkleTools.chunkMerkleTree(hashedLeaves, 8);
+            result_3 = AirDrop.merkleTools.chunkMerkleTree(hashedLeaves, 16);        
         });
 
         it('should produce the same merkle proof from any chunkSize', function() {
@@ -177,54 +192,18 @@ describe('Chunking Merkle', function() {
     describe('checkMerkleProof()', function() {
         var result_0 = {};
         var idx = 10;
-        var merkleRoot = '';
         var proof_0 = [];
-        var mNodes = [];
-        var nNodes = [];
-        var oNodes = [];
 
         before(function() {
-
-            exArray = Array.apply(null, { length: 16 })
-                .map(Number.call, Number)
-                .map(function(x) { return ethers.utils.keccak256('0x' + x); });
-        
-            result_0 = AirDrop.merkleTools.chunkMerkleTree(exArray, 2);
-            
+            result_0 = AirDrop.merkleTools.chunkMerkleTree(hashedLeaves, 2);
             proof_0 = AirDrop.merkleTools.chunkMerkleProof(idx, result_0.chunks[Math.floor(idx/2)], result_0.root);
-
-            mNodes = [
-                ethers.utils.keccak256(exArray[0] + exArray[1].substring(2)),
-                ethers.utils.keccak256(exArray[2] + exArray[3].substring(2)),
-                ethers.utils.keccak256(exArray[4] + exArray[5].substring(2)),
-                ethers.utils.keccak256(exArray[6] + exArray[7].substring(2)),
-                ethers.utils.keccak256(exArray[8] + exArray[9].substring(2)),
-                ethers.utils.keccak256(exArray[10] + exArray[11].substring(2)),
-                ethers.utils.keccak256(exArray[12] + exArray[13].substring(2)),
-                ethers.utils.keccak256(exArray[14] + exArray[15].substring(2))
-            ];
-
-            nNodes = [
-                ethers.utils.keccak256(mNodes[0] + mNodes[1].substring(2)),
-                ethers.utils.keccak256(mNodes[2] + mNodes[3].substring(2)),
-                ethers.utils.keccak256(mNodes[4] + mNodes[5].substring(2)),
-                ethers.utils.keccak256(mNodes[6] + mNodes[7].substring(2)),
-            ];
-
-            oNodes = [
-                ethers.utils.keccak256(nNodes[0] + nNodes[1].substring(2)),
-                ethers.utils.keccak256(nNodes[2] + nNodes[3].substring(2))
-            ]
-
-            merkleRoot = ethers.utils.keccak256(oNodes[0] + oNodes[1].substring(2));
-            
         });
 
         it('should verify the proof matches the merkle root', function() {
 
             assert.isTrue(AirDrop.merkleTools.checkMerkleProof(
                 idx,
-                exArray[idx],
+                hashedLeaves[idx],
                 proof_0,
                 merkleRoot
             ));
